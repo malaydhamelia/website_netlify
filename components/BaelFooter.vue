33b4e6e5746f58c1233ff4711087de167d95a9c4
malaydhamelia <40895432+malaydhamelia@users.noreply.github.com>
2020-04-20 16:11:52 +0530

    Update BaelFooter.vue

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -69,8 +69,7 @@
             </div>
         </div>
         <div class="c-12 xs-text-left xs-p2 xs-border">
-            <div class="item xs-text-6"><a href="https://github.com/jake-101/bael-template">Bael</a>, An open source design by
-                <a href="https://jake101.com">jake101</a>
+            <div class="item xs-text-6"> Website is made using open source design template <a href="https://github.com/jake-101/bael-template">Bael</a>,Vue.js, and netlify for hosting.
             </div>
         </div>
     </div>
